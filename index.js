177ea1eab7ffed0a840acaf268ead98f897d52f9
helinsv <helinsv@gmail.com>
2018-11-11 21:42:49 +0100

    Add data answers

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('Hello world!');
-});
+const answers = [
+	{ 
+		id: 1, 
+		answer:'Заленський',
+		votes: 0
+	},
+	{ 
+		id: 2, 
+		answer:'Порошенко',
+		votes: 0
+	},
+	{ 
+		id: 3, 
+		answer:'Вакарчук',
+		votes: 0
+	},
+];
+
+
+
+
+
+/*app.get('/api/courses/:id', (req, res) => {	
+	const course = courses.find(c => c.id === parseInt(req.params.id));
+	if(!course) res.status(404).send('404');
+	res.send(course.name);
+})*/
+
 
 
 //PORT
